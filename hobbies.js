76d569c03ffd667a470a68a127c3cf03d2890ed7
imnxtamess <davide.morisco.work@gmail.com>
2025-03-10 13:12:20 +0100

    add hobbies function export it and import it on server.js

---
+++ hobbies.js
@@ -0,0 +1,9 @@
+function stringsToArr(str1, str2, str3) {
+  let arr = []
+  arr.push(str1, str2, str3)
+  return arr
+}
+
+console.log(stringsToArr("Motorcycles", "Gaming", "Calisthenics"));
+
+module.exports = stringsToArr
--- server.js
+++ server.js
@@ -1,2 +1,5 @@
 const stringConcat = require("./names")
 stringConcat("Davide", "Morisco")
+
+const stringsToArr = require("./hobbies")
+stringsToArr("Gaming", "Calisthenics", "Motorcycles")
